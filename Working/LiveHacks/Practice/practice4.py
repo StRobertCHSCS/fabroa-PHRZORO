2542a9384935c499f555def7ce16fd35f71ba2d8
PHRZORO <charlie.ma22@ycdsbk12.ca>
2019-09-26 09:38:03 -0400

    n

--- Working/LiveHacks/Practice/practice4.py
+++ Working/LiveHacks/Practice/practice4.py
@@ -1,3 +1,4 @@
 temperature = int(input("Enter the temperature (Celsius): "))
 windspeed = int(input("Enter the wind speed (km/h): "))
-print("The wind chill factor is:", 13.12 + (0.6215 * temperature) - (11.37 * windspeed ^ 0.16) + (0.3965 * temperature * windspeed ^ 0.16))
+print("The wind chill factor is:", end="")
+print(13.12 + (0.6215 * temperature) - (11.37 * windspeed ** 0.16) + (0.3965 * temperature * windspeed ** 0.16))
